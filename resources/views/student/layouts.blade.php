213b2a7c8e5edca1abb121d5ff32c1f483b003a5
Kitajima2910 <kitajima2910@gmail.com>
2020-07-03 19:20:08 +0700

    first commit

---
+++ resources/views/student/layouts.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel 7 CRUD (Create Read Update Delete) Tutorial For Beginners</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+</head>
+<body>
+    
+    <div class="container">
+        @yield('content')
+    </div>
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+</body>
+</html>
